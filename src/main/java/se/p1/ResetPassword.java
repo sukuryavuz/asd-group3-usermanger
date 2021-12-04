41323c437e47992cf009a6dd44c0bb59be10f76a
Milidrag <m-ivkic@gmx.at>
2021-12-04 15:30:34 +0100

    not its possible to update the password

--- src/main/java/se/p1/ResetPassword.java
+++ src/main/java/se/p1/ResetPassword.java
@@ -1,11 +1,82 @@
 package se.p1;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.io.*;
+import java.util.Enumeration;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ResetPassword {
+
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField passwordFieldOld;
+    @FXML
+    private PasswordField passwordFieldNew;
+
     public void handleResetAction(ActionEvent actionEvent) {
+
+        Properties props = readProperties();
+        boolean isValid = isUserValid(props,username.getText(),passwordFieldOld.getText());
+
+        if (isValid){
+            Enumeration keys = props.keys();
+
+            while (keys.hasMoreElements()) {
+                String key = keys.nextElement().toString();
+                if (key.equals(username.getText())){
+                    props.setProperty(key,passwordFieldNew.getText());
+                }
+            }
+
+            try {
+                props.store(new FileOutputStream(new File("password.properties")),null);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
     }
 
-    public void handleSubmitButtonAction(ActionEvent actionEvent) {
+    private Properties readProperties(){
+        File file = new File("password.properties");
+
+        Properties properties = null;
+
+        try (FileInputStream fileInput = new FileInputStream(file)) {
+            properties = new Properties();
+            properties.load(fileInput);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties;
+    }
+
+    private boolean isUserValid(Properties properties, String username, String password) {
+        Enumeration keys = properties.keys();
+
+        while (keys.hasMoreElements()) {
+            String key = keys.nextElement().toString();
+            String value = properties.getProperty(key);
+            if (key.equals(username)){
+                if(value.equals(password) ){
+                    return true;
+                }
+            }
+            return false;
+        }
+        return false;
     }
 }
