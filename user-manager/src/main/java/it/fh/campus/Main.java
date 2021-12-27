e879c654cac9fa93c19766f86cb95ec83112c5bb
schneidermichael <schneidermichael@outlook.com>
2021-12-27 22:35:18 +0100

    removed unused setter

--- user-manager/src/main/java/it/fh/campus/Main.java
+++ user-manager/src/main/java/it/fh/campus/Main.java
@@ -1,7 +1,6 @@
 package it.fh.campus;
 
 import it.fh.campus.service.UserServiceImpl;
-import it.fh.campus.utilities.Rijndael;
 import it.fh.campus.utilities.UserFileHandler;
 
 public class Main {
@@ -10,7 +9,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Rijndael.setKey();
         UserFileHandler.initParser(FILE_PATH);
         new CommandLine(new UserServiceImpl());
     }
